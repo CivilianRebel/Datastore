619e9c1a1e9e1a2ce1035af4b1d6ea7c1e85a43b
civilianrebel <steventayloroo@gmail.com>
2020-05-23 15:44:06 -0400

    docstring updates

--- datastore/disk_store.py
+++ datastore/disk_store.py
@@ -5,8 +5,7 @@ class DiskStore:
 
     def __init__(self, loc):
         """
-        DiskStore can be accessed like a dictionary and stores information on disk
-        'loc/[prefix: optional]/{key}.json
+        Handles higher level reading and writing (key, value) pairs to files on disk
         :type loc: str
         :param loc: base folder to store data, if does not exist it will be created
         """
@@ -14,13 +13,18 @@ class DiskStore:
 
     def __setattr__(self, key, value):
         """
-
-        :param key:
-        :type key:
-        :param value:
-        :type value:
-        :return:
-        :rtype:
+        Writes key, value pair to file
+        uses format: datastore_instance.{key}
+        for example:
+            data.entry1 = 'data1'
+            same as
+            data['entry1'] = 'data1'
+        :param key: Name of key in pair
+        :type key: any
+        :param value: Value to be written to corresponding key
+        :type value: any
+        :return: none
+        :rtype: None
         """
         if key != 'file_helper':
             self.file_helper.set_key(key, value)
@@ -29,33 +33,37 @@ class DiskStore:
 
     def __getattr__(self, item):
         """
-
-        :param item:
-        :type item:
-        :return:
-        :rtype:
+        Gets the json dict for the specified key
+        Uses attribute format for example
+            print(data.entry1)
+            would output same as
+            print(data['entry1'])
+        :param item: Key to grab from disk
+        :type item: str
+        :return: value contained in key file
+        :rtype: any
         """
         return self.file_helper.get_key(item)
 
     def __getitem__(self, item):
         """
-        TODO: Docstring
-        :param item:
-        :type item:
-        :return:
-        :rtype:
+        Same as __getattr__ except with dictionary style
+        :param item: Key to grab value from
+        :type item: str
+        :return: value in key file
+        :rtype: any
         """
         return self.file_helper.get_key(item)
 
     def __setitem__(self, key, value):
         """
-
-        :param key:
-        :type key:
-        :param value:
-        :type value:
-        :return:
-        :rtype:
+        Same as __setattr__ except with dictionary style
+        :param key: Key name to write to disk
+        :type key: str
+        :param value: data to assign to key file
+        :type value: any
+        :return: None
+        :rtype: None
         """
         self.file_helper.set_key(key, value)
 
