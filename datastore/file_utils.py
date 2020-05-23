9b15900633c3a2b9ffca2f70394a1c819743d134
civilianrebel <steventayloroo@gmail.com>
2020-05-23 15:58:38 -0400

    docstring changes and removed redundant method

--- datastore/file_utils.py
+++ datastore/file_utils.py
@@ -7,29 +7,33 @@ from datastore.key import Key
 
 class FileHelper:
 
-    def __init__(self, base, *args, **kwargs):
+    def __init__(self, base, *args, data_folder_name='data', **kwargs):
         """
-        TODO: Docstring
-        :param base:
-        :type base:
-        :param args:
-        :type args:
-        :param kwargs:
-        :type kwargs:
+        Handles most of the file checking and some medium level key, value pair methods
+        Ensures base and data_folder_name directories exist
+        Some string building for folder/file paths that may be moved later
+
+        :param base: Folder to store data in, note another directory will be created with
+                     the name data_folder_name inside of 'base' directory
+        :type base: str
+        :param args: Extra positional arguments, none are handled as of now
+        :type args: list
+        :param kwargs: Extra keyword arguments, none are handled as of now
+        :type kwargs: dict
         """
         self.base = base
         self.done_init = False
         self.full_path = None
-        self.data_folder_name = kwargs.get('data_folder_name', 'data')
+        self.data_folder_name = data_folder_name
         self.initialize(*args, **kwargs)
 
     def initialize(self, *args, **kwargs):
         """
-        TODO: Docstring
-        :param args:
-        :type args:
-        :param kwargs:
-        :type kwargs:
+        Create data and base folders if they do not exist
+        :param args: Not handled
+        :type args: list
+        :param kwargs: Not handled
+        :type kwargs: dict
         """
         if not exists(self.base):
             self.create_structure('all')
@@ -39,9 +43,11 @@ class FileHelper:
 
     def create_structure(self, *args):
         """
-        TODO: Docstring
-        :param args:
-        :type args:
+        Create folder structure according to args strings
+        :param args: List of strings specifying which folders to create
+                     'all': creates entire folder structure from scratch
+                     'data': only creates data_folder_name inside of base dir
+        :type args: list
         """
         if 'all' in args:
             os.mkdir(self.base)
@@ -51,11 +57,11 @@ class FileHelper:
 
     def set_key(self, k, v):
         """
-        TODO: Docstring
-        :param k:
-        :type k:
-        :param v:
-        :type v:
+        Set key(k) to value(v)
+        :param k: Name of key
+        :type k: str
+        :param v: Value to write to key
+        :type v: any
         """
         k_file = Key(k, data_path=self.data_path)
         value = Value(k_file, v)
@@ -63,11 +69,11 @@ class FileHelper:
 
     def get_key(self, k):
         """
-        TODO: Docstring
-        :param k:
-        :type k:
-        :return:
-        :rtype:
+        Returns the value of the requested key(k)
+        :param k: Key to read from disk
+        :type k: str
+        :return: Value stored, throws KeyError if none exist
+        :rtype: any
         """
         k_file = Key(k, data_path=self.data_path)
         v = Value(key=k_file)
@@ -76,27 +82,18 @@ class FileHelper:
     @property
     def data_path(self):
         """
-        TODO: Docstring
+        Returns string for the path to store json files in
         :return:
         :rtype:
         """
         return join(self.base, self.data_folder_name)
 
     @property
-    def path(self):
-        """
-        TODO: Docstring
-        :return:
-        :rtype:
-        """
-        return self.data_path
-
-    @property
     def glob_all(self):
         """
-        TODO: Docstring
-        :return:
-        :rtype:
+        Grabs list of all json files in data path
+        :return: List of json file paths for all key entries stored on disk
+        :rtype: list
         """
         return glob.glob(join(self.data_path, '*.json'))
 
